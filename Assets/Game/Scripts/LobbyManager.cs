2139e82e9fbd70544edcd558a97442ac51154c43
Linkchacar <82171873+Linkchacar@users.noreply.github.com>
2024-07-10 18:23:24 -0400

    Goood

--- Assets/Game/Scripts/LobbyManager.cs
+++ Assets/Game/Scripts/LobbyManager.cs
@@ -73,7 +73,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     public void Update()
     {
-        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount == 2)
         {
             playButton.SetActive(true);
         }
